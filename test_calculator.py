55dae45639272ca5246fa4eb637fad7c59666ed9
Anthony Zheng <anthonyzheng124@gmail.com>
2025-04-10 15:58:59 -0400

    test_calc p1

--- test_calculator.py
+++ test_calculator.py
@@ -12,18 +12,19 @@ class TestCalculator(unittest.TestCase):
 
     # def test_subtract(self): # 3 assertions
     #     fill in code
-    # ##########################
 
     ####### Partner 1
     def test_multiply(self): # 3 assertions
-        self.assertEqual(mul(2, 5))
-        self.assertAlmostEqual(mul(1.5, 2.25))
-        self.assertEqual(mul(-1,5))
+        self.assertEqual(mul(2, 5),10)
+        self.assertAlmostEqual(mul(1.5, 2.25),3.375)
+        self.assertEqual(mul(-1,5),-5)
 
     def test_divide(self): # 3 assertions
-        self.assertAlmostEqual(div(10,5))
-        self.assertAlmostEqual(div(0,0))
-        self.assertAlmostEqual(div(-1,2))
+        with self.assertRaises(ValueError):
+            div(0,0)
+        self.assertAlmostEqual(div(10,5),2)
+        self.assertAlmostEqual(div(-1,2),-0.5)
+        self.assertEqual(div(50,25),2)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -43,20 +44,20 @@ class TestCalculator(unittest.TestCase):
     ####### Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         with self.assertRaises(ValueError):
-            logarithm(0,5)
+            logarithm(0,0)
 
     def test_hypotenuse(self): # 3 assertions
-        self.assertAlmostEqual(hypotenuse(1,5))
-        self.assertAlmostEqual(hypotenuse(-11,5))
-        self.assertAlmostEqual(hypotenuse(1,0.5))
+        self.assertAlmostEqual(hypotenuse(3,4),5)
+        self.assertAlmostEqual(hypotenuse(5,12),13)
+        self.assertAlmostEqual(hypotenuse(9,12),15)
 
 
     def test_sqrt(self): # 3 assertions
         with self.assertRaises(ValueError):
-            square_root(a)
-        self.assertEqual(square_root(1,2))
-        self.assertEqual(square_root(5,-2))
-        self.assertEqual(0.5,5)
+            square_root(-1)
+        self.assertEqual(square_root(9),3)
+        self.assertEqual(square_root(25),5)
+        self.assertEqual(square_root(4),2)
 
 # Do not touch this
 if __name__ == "__main__":
